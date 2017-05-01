b3c41d86eb07b615b21966ab8117b2fd63c29880
Bob Willer <bob.willer@hotmail.com>
2017-04-30 22:50:04 -0400

    sunday night

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,39 @@
 import { Component } from '@angular/core';
 
+interface Child {
+  name: string,
+  age: number
+}
+
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean,
+  checkInDate: number | null,
+  children: Child[] | null
+}
+
+
+
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+      <passenger-dashboard></passenger-dashboard>
     </div>
-  `
+    `
 })
 export class AppComponent {
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephen',
+    checkedIn: true,
+    checkInDate: 149074200000,
+    children: null
+  }]
 
+ 
+
+  
 }
